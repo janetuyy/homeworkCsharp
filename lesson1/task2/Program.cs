a2a67aaa5bd352ccf63c7506fc81fe00d5cd2501
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-22 21:27:57 +0300

    mr1

---
+++ lesson1/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+    Console.WriteLine("u can write 3 int numbers");
+     Console.Write("a=");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("b=");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.Write("c=");
+    int c = Convert.ToInt32(Console.ReadLine());
+    
+    if (a > b)
+{
+    if (a > c)
+            Console.WriteLine("max= " + a);
+}
+
+if (b > c)
+{
+    if (b > a)
+            Console.WriteLine("max= " + b);
+}
+
+if (c > b)
+{
+    if (c > a)
+        Console.WriteLine("max= " + c);
+}
+   

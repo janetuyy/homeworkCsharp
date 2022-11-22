5808462c012c8755cfb43ad41f8825496f3ebaec
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-22 23:42:04 +0300

    mrr3.1

---
+++ lesson3/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Hello, write 5 char num");
+string a=Console.ReadLine();
+if (a.Length!=5)
+    Console.WriteLine("eror: incorrect value");
+else
+    if (a[1]==a[3] && a[0]==a[4])
+        Console.WriteLine("yes");
+    else
+        Console.WriteLine("no");

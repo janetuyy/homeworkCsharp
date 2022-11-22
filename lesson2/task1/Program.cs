b681a7b7f1e4b6bd365b758a3994a51c73998684
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-22 22:25:47 +0300

    mr2.1

---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("write 3th char numer");
+int a=0;
+try
+{
+a=Convert.ToInt32(Console.ReadLine());
+}
+catch(Exception)
+{
+    Console.WriteLine();
+    Console.WriteLine("Ошибка: Не корректное значение");
+    Environment.Exit(0);
+}
+a=a/10;
+a=a%10;
+Console.WriteLine($"num={a}");

5b6547bd91761ab6f2a0f4dc7edc1ff92403564a
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-23 00:10:06 +0300

    mrr3.2

---
+++ lesson3/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Hello, World!");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+
+double number = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+string result = string.Format("{0:C2}", number);
+Console.WriteLine(result);

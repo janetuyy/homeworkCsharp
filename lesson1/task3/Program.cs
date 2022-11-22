efaafc927dd5e20ad4ec7fd390c6f49a2241fa11
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-22 22:00:26 +0300

    mrr2

---
+++ lesson1/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("write a int");
+Console.Write("a=");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2==0)
+{
+    Console.WriteLine("yes");
+}
+if (a%2!=0)
+{
+    Console.WriteLine("no");
+}
---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, write an int !");
+int a=Convert.ToInt32(Console.ReadLine());
+string s="";
+for (int i = 1; i <= a; i++)
+    if (i%2==0)
+        s+=$"{i},";
+s=s.TrimEnd(' ');
+s=s.TrimEnd(',');
+Console.WriteLine(s);

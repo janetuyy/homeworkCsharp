34b6b8af336a2efcc354e38c65acca8d1a0fa189
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-22 22:57:23 +0300

    mrr2.2

---
+++ lesson2/task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Hello, write num!");
+//int a=0;
+//a=Convert.ToInt32(Console.ReadLine());
+string s=Console.ReadLine();
+if (s.Length<3)
+    Console.WriteLine("nothing to see here");
+else
+    Console.WriteLine(s[2]);

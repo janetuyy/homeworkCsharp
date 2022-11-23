565410512e0b2a8c4f5037669cf5df942e48cbdc
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-24 00:41:52 +0300

    mur4.2

---
+++ lesson4/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int sum(int a)
+{
+    int s=0;
+    while (a>0)
+        {
+            s+=a%10;
+            a/=10;
+        }
+        return s;
+}
+Console.WriteLine("Hello, write me ur num!");
+Console.Write("a=");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(a));

e5305c5e536cf407fe2705aedb71eb021dabc87e
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-25 00:51:53 +0300

    murmeow5.3

---
+++ lesson5/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.WriteLine("Hello, World!");
+string n = "";
+float[] array = new float[5];
+for (int i = 0; i < 5; i++)
+{
+    array[i] = Convert.ToSingle(Console.ReadLine());
+}
+float max = array.Max();
+float min = array.Min();
+
+Console.WriteLine(max - min);

9dd3a8fdfb9dc0954ed2b816950e0897f80a377b
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-25 00:35:25 +0300

    murmeow5.2

---
+++ lesson5/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("Hello, i'll tell u smthng!");
+int sum =0;
+string n="";
+int[] array = new int[4];
+for (int i=0; i<4; i++)
+{
+    array[i] =  new Random().Next(0, 100);
+    if (i%2==0)
+    {
+        sum+=array[i];
+    }
+}
+foreach (double item in array)
+{
+n += $"{item}, ";
+}
+n=n.TrimEnd(' ');
+n=n.TrimEnd(',');
+
+Console.WriteLine(n);
+
+Console.WriteLine(sum);

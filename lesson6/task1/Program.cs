5872582da0bc3e3d27664c1c57634b09a9a13bf8
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-25 23:56:57 +0300

    mrmeow6.1

---
+++ lesson6/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Hey, how many num will u write to me?");
+int res = 0;
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+Console.WriteLine("Hey, write ur numbers");
+
+for (int i = 0; i < n; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if (arr[i] > 0)
+    {
+        res += 1;
+    }
+}
+Console.WriteLine(res);

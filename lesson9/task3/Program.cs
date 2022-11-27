2371b1d669b0ccd0ef39eb33ee2a97d3c7fb6eb1
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-11-27 21:37:53 +0300

    finallymrmews9.3

---
+++ lesson9/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+Console.WriteLine("Hello, World!");
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int A(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return A(n - 1, 1);
+    if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+    return A(n, m);
+}
+Console.WriteLine(A(n, m));
